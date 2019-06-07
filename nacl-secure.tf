2d2e16031dc741060b2a4de11d574d0d3913d835
Allan Denot <adenot@gmail.com>
2019-06-07 11:18:55 +1000

    Wrong cidr_bock for nacl and missing secure to secure rule

--- nacl-secure.tf
+++ nacl-secure.tf
@@ -37,23 +37,33 @@ resource "aws_network_acl_rule" "out_secure_to_private" {
 }
 
 resource "aws_network_acl_rule" "out_secure_to_transit" {
-  count          = "${var.transit_subnet ? length(aws_subnet.private.*.cidr_block) : 0}"
+  count          = "${var.transit_subnet ? length(aws_subnet.transit.*.cidr_block) : 0}"
   network_acl_id = "${aws_network_acl.secure.id}"
   rule_number    = "${count.index + 201}"
   egress         = true
   protocol       = -1
   rule_action    = "allow"
-  cidr_block     = "${aws_subnet.private.*.cidr_block[count.index]}"
+  cidr_block     = "${aws_subnet.transit.*.cidr_block[count.index]}"
 }
 
 ###########
 # INGRESS
 ###########
 
+resource "aws_network_acl_rule" "in_secure_from_secure" {
+  count          = "${length(aws_subnet.secure.*.cidr_block)}"
+  network_acl_id = "${aws_network_acl.secure.id}"
+  rule_number    = "${count.index + 101}"
+  egress         = false
+  protocol       = -1
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.secure.*.cidr_block[count.index]}"
+}
+
 resource "aws_network_acl_rule" "in_secure_from_private" {
   count          = "${length(aws_subnet.private.*.cidr_block)}"
   network_acl_id = "${aws_network_acl.secure.id}"
-  rule_number    = "${count.index + 101}"
+  rule_number    = "${count.index + 201}"
   egress         = false
   protocol       = -1
   rule_action    = "allow"
@@ -63,7 +73,7 @@ resource "aws_network_acl_rule" "in_secure_from_private" {
 resource "aws_network_acl_rule" "in_secure_from_transit" {
   count          = "${var.transit_subnet ? length(aws_subnet.transit.*.cidr_block) : 0}"
   network_acl_id = "${aws_network_acl.secure.id}"
-  rule_number    = "${count.index + 201}"
+  rule_number    = "${count.index + 301}"
   egress         = false
   protocol       = -1
   rule_action    = "allow"
