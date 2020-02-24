120cc7681b6004a12647ad3c52bb4641d964c81e
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2020-02-24 15:24:56 +1100

    include join function to include all values no just the first element

--- cf-exports.tf
+++ cf-exports.tf
@@ -6,13 +6,13 @@ resource "aws_cloudformation_stack" "tf_exports" {
       "VpcId"              = aws_vpc.default.id,
       "CidrBlock"          = aws_vpc.default.cidr_block,
       "InternetGatewayId"  = aws_internet_gateway.default.id,
-      "PublicSubnetIds"    = aws_subnet.public.*.id[0],
-      "PublicSubnetCidrs"  = aws_subnet.public.*.cidr_block[0],
-      "PrivateSubnetIds"   = aws_subnet.private.*.id[0],
-      "PrivateSubnetCidrs" = aws_subnet.private.*.cidr_block[0],
-      "SecureSubnetIds"    = aws_subnet.secure.*.id[0],
-      "SecureSubnetCidrs"  = aws_subnet.secure.*.cidr_block[0],
-      "NatGatewayIds"      = aws_nat_gateway.nat_gw.*.id[0],
+      "PublicSubnetIds"    = join(",", aws_subnet.public.*.id),
+      "PublicSubnetCidrs"  = join(",", aws_subnet.public.*.cidr_block),
+      "PrivateSubnetIds"   = join(",",aws_subnet.private.*.id),
+      "PrivateSubnetCidrs" = join(",",aws_subnet.private.*.cidr_block),
+      "SecureSubnetIds"    = join(",",aws_subnet.secure.*.id),
+      "SecureSubnetCidrs"  = join(",",aws_subnet.secure.*.cidr_block),
+      "NatGatewayIds"      = join(",",aws_nat_gateway.nat_gw.*.id),
       "DbSubnetGroupId"    = aws_db_subnet_group.secure.id
     }
   })
