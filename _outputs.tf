1604c4d6a8889968bc6844319b9d41a537bf95bc
felipegfalcao <felipegfalcao.si@gmail.com>
2020-09-09 00:40:00 -0300

    update _output.tf
    Output empty tuple ex eg. [[]]

--- _outputs.tf
+++ _outputs.tf
@@ -14,37 +14,37 @@ output "internet_gateway_id" {
 }
 
 output "public_subnet_ids" {
-  value       = [aws_subnet.public.*.id]
+  value       = aws_subnet.public.*.id
   description = "List of public subnet IDs"
 }
 
 output "public_subnet_cidrs" {
-  value       = [aws_subnet.public.*.cidr_block]
+  value       = aws_subnet.public.*.cidr_block
   description = "List of public subnet CIDRs"
 }
 
 output "private_subnet_ids" {
-  value       = [aws_subnet.private.*.id]
+  value       = aws_subnet.private.*.id
   description = "List of private subnet IDs"
 }
 
 output "private_subnet_cidrs" {
-  value       = [aws_subnet.private.*.cidr_block]
+  value       = aws_subnet.private.*.cidr_block
   description = "List of private subnet CIDRs"
 }
 
 output "secure_subnet_ids" {
-  value       = [aws_subnet.secure.*.id]
+  value       = aws_subnet.secure.*.id
   description = "List of secure subnet IDs"
 }
 
 output "secure_subnet_cidrs" {
-  value       = [aws_subnet.secure.*.cidr_block]
+  value       = aws_subnet.secure.*.cidr_block
   description = "List of secure subnet CIDRs"
 }
 
 output "nat_gateway_ids" {
-  value       = [aws_nat_gateway.nat_gw.*.id]
+  value       = aws_nat_gateway.nat_gw.*.id
   description = "List of NAT Gateway IDs"
 }
 
