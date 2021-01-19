2b5e6a582f45b65118938a010ec1238f1d1c8e75
José Roberto Prado <jrpradojunior@gmail.com>
2021-01-19 03:37:44 -0300

    Fix subnet private

--- subnet-private.tf
+++ subnet-private.tf
@@ -41,7 +41,8 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "nat_route" {
-  count                  = var.multi_nat ? length(data.aws_availability_zones.available.names) > var.max_az ? var.max_az : length(data.aws_availability_zones.available.names) : var.nat_gw ? 1 : 0
+  # count                  = var.multi_nat ? length(data.aws_availability_zones.available.names) > var.max_az ? var.max_az : length(data.aws_availability_zones.available.names) : var.nat_gw ? 1 : 0
+  count = var.nat_gw ? var.multi_nat ? length(data.aws_availability_zones.available.names) > var.max_az ? var.max_az : length(data.aws_availability_zones.available.names) : 1 : 0
   route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat_gw[count.index].id
