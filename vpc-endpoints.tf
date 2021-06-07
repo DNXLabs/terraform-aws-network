4a7b75fe0b06315657e74608425bf3894daae663
maicon.rocha@dnx.solutions <maicon.rocha@dnx.solutions>
2021-06-07 04:06:50 +0000

    restrict permissions on VPC endpoint to only within the VPC

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "vpc_endpoints" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
