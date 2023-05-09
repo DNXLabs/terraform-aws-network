d6ca69a6d69d81d4fac2929cb693fa4c9adf9521
Renato <renato@dnx.solutions>
2023-05-09 11:01:25 +1000

    🔧 fixing default flow in case  vpc_cidr_summ is not defined

--- nacl-secure.tf
+++ nacl-secure.tf
@@ -1,6 +1,6 @@
 locals {
   secure_subnet_ip = split("/",aws_subnet.secure.cidr_block)[0]
-  secure_subnet_summary = var.vpc_cidr_summ != "/0" ? cidrhost("${secure_subnet_ip}${var.vpc_cidr_summ}", 0) : aws_subnet.secure.cidr_bloc
+  secure_subnet_summary = var.vpc_cidr_summ != "/0" ? cidrhost("${secure_subnet_ip}${var.vpc_cidr_summ}", 0) : aws_vpc.default.cidr_block
 }
 resource "aws_network_acl" "secure" {
   vpc_id     = aws_vpc.default.id
