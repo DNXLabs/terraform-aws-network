98146c0b91488f3518221af87726de184332bdb1
Renato <renato@dnx.solutions>
2023-05-09 11:03:17 +1000

    📝 Improve description of vpc_cidr_summ var

--- _variables.tf
+++ _variables.tf
@@ -39,7 +39,7 @@ variable "newbits" {
 variable "vpc_cidr_summ" {
   type        = string
   default     = "/0"
-  description = "Define cidr used to summuary subnets"
+  description = "Define cidr used to summarize subnets by tier"
 }
 
 variable "tags" {
