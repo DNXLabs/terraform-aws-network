c81be4c5cb7cffe30545ea456e61433b5d8dc633
Caio Fernandes <42192251+caiovfernandes@users.noreply.github.com>
2025-03-31 19:16:10 -0300

    Add s3 gateway endpoint association

--- vpc-endpoint-s3.tf
+++ vpc-endpoint-s3.tf
@@ -21,7 +21,10 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private" {
-  count           = var.vpc_endpoint_s3_gateway ? length(aws_subnet.private) : 0
-  route_table_id  = var.multi_nat ? aws_route_table.private[count.index].id : aws_route_table.private[0].id
+  for_each = var.vpc_endpoint_s3_gateway ? {
+    for idx, subnet in aws_subnet.private : idx => subnet
+  } : {}
+
+  route_table_id  = var.multi_nat || var.multi_az_private_rtb ? aws_route_table.private[each.key].id : aws_route_table.private[0].id
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
 }
