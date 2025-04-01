ceed40f76dacec1855a4c6bf6b5c41c1513d08e2
Caio Fernandes <caiovf13@gmail.com>
2025-04-01 03:33:41 -0300

    Fix db-subnet Warning - List items should be accessed using square brackets (terraform_deprecated_index)

--- db-subnet.tf
+++ db-subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "secure" {
   count      = var.create_dbsubgroup_secure ? 1 : 0
   name       = var.db_subnet_group_secure_name_compat ? lower("${var.name}-dbsubnet") : lower("${format(local.names[var.name_pattern].db_subnet, var.name, local.name_suffix)}-secure")
-  subnet_ids = aws_subnet.secure.*.id
+  subnet_ids = aws_subnet.secure[*].id
 
   tags = merge(
     var.tags,
@@ -16,7 +16,7 @@ resource "aws_db_subnet_group" "secure" {
 resource "aws_db_subnet_group" "private" {
   count      = var.create_dbsubgroup_private ? 1 : 0
   name       = lower("${format(local.names[var.name_pattern].db_subnet, var.name, local.name_suffix)}-private")
-  subnet_ids = aws_subnet.private.*.id
+  subnet_ids = aws_subnet.private[*].id
 
   tags = merge(
     var.tags,
@@ -31,7 +31,7 @@ resource "aws_db_subnet_group" "private" {
 resource "aws_db_subnet_group" "public" {
   count      = var.create_dbsubgroup_public ? 1 : 0
   name       = lower("${format(local.names[var.name_pattern].db_subnet, var.name, local.name_suffix)}-public")
-  subnet_ids = aws_subnet.public.*.id
+  subnet_ids = aws_subnet.public[*].id
 
   tags = merge(
     var.tags,
